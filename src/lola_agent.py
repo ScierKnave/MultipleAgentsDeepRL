31a8d0f129337c138629d2b206cf8e9488331115
Dereck Piché <pichedereck@gmail.com>
2024-04-15 11:39:37 -0400

    test

--- src/lola_agent.py
+++ src/lola_agent.py
@@ -1,12 +1,9 @@
-
-
 class lola_agent(nn.Module):
     def __init__(self):
         self.Super.__init__(self)
-        self.optimizer = torch.AdamW(self) 
+        self.optimizer = torch.AdamW(self)
 
     def introspective_loss(self):
-        
-    
-    def intraspective_loss(self):
 
+
+    def intraspective_loss(self):
--- src/trajectories.py
+++ src/trajectories.py
@@ -1,5 +1,21 @@
 import torch
 
+def collect_trajectories(env, nb, policy_x, policy_y):
+    trajectories = {}
+    trajectories['states'] = []
+    trajectories['actions_x'] = []
+    trajectories['actions_y'] = []
+    trajectories['rewards_x'] = []
+    trajectories['rewards_y'] = []
+    for _ in range(nb):
+        states, actions_x, actions_y, rewards_x, rewards_y = collect_trajectory(env, policy_x, policy_y)
+        trajectories['states'].append(states)
+        trajectories['actions_x'].append(actions_x)
+        trajectories['actions_y'].append(actions_y)
+        trajectories['rewards_x'].append(rewards_x)
+        trajectories['rewards_y'].append(rewards_y)
+    return trajectories
+
 def collect_trajectory(env, policy_x, policy_y):
     state = env.reset()
     done = False
@@ -8,11 +24,11 @@ def collect_trajectory(env, policy_x, policy_y):
         action_x = torch.argmax(policy_x(state))
         action_y = torch.argmax(policy_y(state))
         state, reward_x, reward_y, done, _ = env.step(action_x, action_y)
-        states.append[state]
-        actions_x.append[action_x] 
-        actions_y.append[action_y] 
-        rewards_x.append[reward_x]
-        rewards_y.append[reward_y]
+        states.append(state)
+        actions_x.append(action_x)
+        actions_y.append(action_y)
+        rewards_x.append(reward_x)
+        rewards_y.append(reward_y)
     return states, actions_x, actions_y, rewards_x, rewards_y
 
 
@@ -21,10 +37,7 @@ def lola_pg(env, trajectories, policy_x, policy_y, policy_y_star):
         states, actions_x, actions_y, rewards_x, rewards_y = trajectory
 
         lola_term = TODO
-        # 
+        #
         logits = policy_x(states)
         acs_dict = torch.distributions.Categoriacal(logits)
-        loss = 
-
-
-    
+        loss =
